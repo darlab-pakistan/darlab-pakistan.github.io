e19894a0f6823e8a9dc632a6a048340db050118b
mqpasta <mqpasta@gmail.com>
2024-10-27 20:22:21 +0500

    added animal-abuse-pakistan repo

--- update/pakistan-just-data.cmd
+++ update/pakistan-just-data.cmd
@@ -1,2 +1,3 @@
 git subtree pull --prefix pakistan-justice-data https://github.com/darlab-pakistan/pakistan-justice-data.git main --squash
 git subtree pull --prefix research-ready-covid19 https://github.com/darlab-pakistan/research-ready-covid19.git main --squash
+git subtree pull --prefix animal-abuse-pakistan https://github.com/darlab-pakistan/animal-abuse.git main --squash
