c278a92247b0572d1604a03044ff6b725d278dbf
mqpasta <mqpasta@gmail.com>
2024-11-29 18:47:17 +0500

    adding karachi-bus-network

--- update/pakistan-just-data.cmd
+++ update/pakistan-just-data.cmd
@@ -2,3 +2,4 @@ git subtree pull --prefix pakistan-justice-data https://github.com/darlab-pakist
 git subtree pull --prefix research-ready-covid19 https://github.com/darlab-pakistan/research-ready-covid19.git main --squash
 git subtree pull --prefix animal-abuse-pakistan https://github.com/darlab-pakistan/animal-abuse.git main --squash
 git subtree pull --prefix koh-e-atlas https://github.com/darlab-pakistan/koh-e-atlas.git main --squash
+git subtree pull --prefix karachi-bus-network https://github.com/darlab-pakistan/karachi-bus-network.git main --squash
